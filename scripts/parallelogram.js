91bcbf39a29eb7e9284712ba00ffd6ab79848301
emon-hossain1001 <mrx00001001@gmail.com>
2024-02-13 17:10:44 +0600

    added more js file

--- scripts/parallelogram.js
+++ scripts/parallelogram.js
@@ -1,21 +1,46 @@
+// function calculateParallelogramArea() {
+//     // get parallelogram base value
+//     const baseInput = document.getElementById('parallelogram-base');
+//     const baseText = baseInput.value;
+//     const base = parseFloat(baseText);
+//     console.log(base);
+
+//     // get parallelogram height
+//     const heightInput = document.getElementById('parallelogram-height');
+//     const heightText = heightInput.value;
+//     const height = parseFloat(heightText);
+//     console.log(height);
+
+//     // calculate area
+//     const area = base * height;
+//     console.log('area of parallelogram is ',area);
+
+//     // display parallelogram area
+//     const parallelogramAreaSpan = document.getElementById('parallelogram-area');
+//     parallelogramAreaSpan.innerText = area;
+// }
+
 function calculateParallelogramArea() {
-    // get parallelogram base value
-    const baseInput = document.getElementById('parallelogram-base');
-    const baseText = baseInput.value;
-    const base = parseFloat(baseText);
-    console.log(base);
+    const base = getInputValueById('parallelogram-base');
+    // console.log('base value', base);
 
-    // get parallelogram height
-    const heightInput = document.getElementById('parallelogram-height');
-    const heightText = heightInput.value;
-    const height = parseFloat(heightText);
-    console.log(height);
+    const height = getInputValueById('parallelogram-height');
+    // console.log(height);
 
-    // calculate area
     const area = base * height;
-    console.log('area of parallelogram is ',area);
+    console.log('area of the parallelogram is ',area);
 
-    // display parallelogram area
-    const parallelogramAreaSpan = document.getElementById('parallelogram-area');
-    parallelogramAreaSpan.innerText = area;
+    setInnerTextById('parallelogram-area',area);
+}
+
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    return inputValue;
+}
+
+function setInnerTextById(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
 }
